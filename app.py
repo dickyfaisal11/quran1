48b2a71d40a027f03bb5c6e433603c1ef46495ed
dicky faisal <faisaldicky77@gmail.com>
2025-04-25 12:36:39 +0700

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ import os
 
 app = Flask(__name__)
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
+
+
 # Fungsi untuk mendapatkan daftar surah
 def get_surah_list():
     try:
